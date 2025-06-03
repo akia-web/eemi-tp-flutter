6abf9a2994bbca547ceebd72f54d47889d72cbbb
akia-web <ch.royer15@gmail.com>
2025-06-03 09:41:31 +0200

    Initial commit

---
+++ lib/class/product.dart
@@ -0,0 +1,25 @@
+class ProductModel {
+  final String id;
+  final String name;
+  final String description;
+  final String price;
+  final List image;
+
+  const ProductModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.image,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      price: json['price'],
+      image: json['image'],
+    );
+  }
+}
